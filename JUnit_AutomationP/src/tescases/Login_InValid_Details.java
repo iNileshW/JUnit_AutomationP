c35082084901db55a27f8d7a959d9af5d3892117
Nilesh <33792419+iNileshW@users.noreply.github.com>
2018-05-19 22:54:41 +0100

    Initial Commit

---
+++ JUnit_AutomationP/src/tescases/Login_InValid_Details.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package tescases;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * @author Nilesh
+ *
+ */
+public class Login_InValid_Details {
+	WebDriver driver;
+	
+	@Before 
+	public void before() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com");
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+	}
+	@Test
+	public void test() {
+		String Title = driver.getTitle(); 
+		System.out.println(Title);
+		driver.findElement(By.linkText("Sign in")).click();
+		Title = driver.getTitle();
+		System.out.println(Title);
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("mymail@aol.com");
+		driver.findElement(By.name("passwd")).clear();
+		driver.findElement(By.name("passwd")).sendKeys("123456");
+		driver.findElement(By.id("SubmitLogin")).click();
+		String loginmsg=driver.findElement(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li")).getText();
+		System.out.println(loginmsg);
+	}
+	
+	@After
+	public void after() {
+		driver.quit();
+	}
+
+}
